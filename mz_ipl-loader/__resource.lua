9ebc561440d2a0136fd66a58b4393dafff3195c2
MasterZyper <master.zyper@web.de>
2019-08-27 20:35:10 +0200

    Added Code

---
+++ mz_ipl-loader/__resource.lua
@@ -0,0 +1,84 @@
+﻿--[[
+MZ-IPL-Loader, controls the Loading of Map-IPL´s in FiveM
+Copyright (C) 27.08.2019  MasterZyper 🐦
+Contact: masterzyper@reloaded-server.de
+
+This program is free software; you can redistribute it and/or modify it under the terms of the 
+GNU General Public License as published by the Free Software Foundation; either version 3 of 
+the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; 
+if not, see <http://www.gnu.org/licenses/>.
+]]
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+client_script 'ipl-loader_client.net.dll';
+
+--Config:
+--	0 = Dekaktiviert 
+--	1 = Aktiviert
+
+-- load_carrier -> Flugzeugträger
+load_carrier '1'
+-- load_heist_yacht -> Yacht in der Bucht von LosSantos
+load_heist_yacht '1'
+-- load_gunrunning_yacht -> Yacht vom GunRunning Update
+load_gunrunning_yacht '1'
+--load_simeon -> Simeon´s Autohaus: -47.16170 - 1115.3327 26.5
+load_simeon '1'
+--load_trevor_trailer -> Trevor´s Trailer: 1985.48132, 3828.76757, 32.5
+load_trevor_trailer '1'
+--load_maxrenda -> MaxRenda: -585.8247, -282.72, 35.45475
+load_maxrenda '1'
+--load_cluckingbell -> Cluckin Bell Fabrik: -146.3837, 6161.5, 30.2062
+load_cluckingbell '1'
+--load_des_farm -> Grapeseed's farm: 2447.9, 4973.4, 47.7
+load_des_farm '1' 
+--load_fib_lobby -> FIB lobby: 105.4557, -745.4835, 44.7548
+load_fib_lobby '1'
+--load_billboard_ifruit -> Billboard: iFruit
+load_billboard_ifruit '1'
+--load_lesterfactory -> Lester's factory: 716.84, -962.05, 31.59
+load_lesterfactory '1'
+--Load_lifeinvaderlobby -> Life Invader lobby: -1047.9, -233.0, 39.0
+load_lifeinvaderlobby '1'
+--load_tunnel -> Tunel Hole
+load_tunnel '1'
+--load_stadiumfameorshame -> Stadium "Fame or Shame": -248.49159240722656, -2010.509033203125, 34.57429885864258
+load_stadiumfameorshame '0'
+--load_houseinbanhamcanyon -> House in Banham Canyon: -3086.428, 339.2523, 6.3717
+load_houseinbanhamcanyon '1'
+--Load_loststrailerpark -> Lost's trailer park: 49.49379000, 3744.47200000, 46.38629000
+load_loststrailerpark '1'
+--load_ratoncanyonriver -> Raton Canyon river: -1652.83, 4445.28, 2.52
+load_ratoncanyonriver '1'
+--load_pillboxhospital -> Pillbox hospital
+load_pillboxhospital '1'
+--load_lostsafehouse -> Lost safehouse: 984.1552, -95.3662, 74.50
+load_lostsafehouse '1'
+--load_redcarpet -> Red Carpet: 300.5927, 199.7589, 104.3776
+load_redcarpet '0'
+--load_deshospital -> Destroyed hosptal 356.8,  -590.1, 43.3
+load_deshospital '0'
+--load_sunkencargoship -> Load the sunken cargoship: - 90.0, -2365.8, 14.3
+load_sunkencargoship '0'
+--load_jewelrystore -> Jewelry Store  -630.4, -236.7, 40.0
+load_jewelrystore '1'
+--load_traincrash -> Train Crash -532 4526 88.79
+load_traincrash '0'
+--load_brokenstilthouse -> Broken Stilt House - 1020.5 663.41 154.75
+load_brokenstilthouse '0'
+--load_planecrash -> PlaneCrash 2814.7 4758.5 50.0
+load_planecrash '0'
+--load_northyankton -> North Yankton: 3217.697, -4834.826, 111.8152
+load_northyankton '0'
+--load_onlinebunkers -> Online Bunkers
+load_onlinebunkers '1'
+--load_arcadiusbusinesscentre ->
+load_arcadiusbusinesscentre '0'
+--load_morgue -> Morgue (Leichenschauhaus)
+load_morgue '1'
