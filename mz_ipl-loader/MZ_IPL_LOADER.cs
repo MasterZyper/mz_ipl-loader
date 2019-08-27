a5194e5b0428fc04a2cf98fb7b2f8f4d552b9e72
MasterZyper <master.zyper@web.de>
2019-08-27 20:30:11 +0200

    Projektdateien hinzufügen.

---
+++ mz_ipl-loader/MZ_IPL_LOADER.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mz_ipl_loader
+{
+    public class MZ_IPL_LOADER
+    {
+
+    }
+}
